e734f9a86a29fe8edf0fc09b210bf2572eb23dba
Norman <norman.seidel@gmail.com>
2019-05-16 12:03:44 +0200

    Add some logging to SampleController

--- src/main/kotlin/de/kotlin/spring/SampleController.kt
+++ src/main/kotlin/de/kotlin/spring/SampleController.kt
@@ -2,12 +2,16 @@ package de.kotlin.spring
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 @RestController
 class SampleController {
+    private val LOG: Logger = LoggerFactory.getLogger(SampleController::class.java)
 
     @GetMapping("/")
     fun hello(): Greeting {
+        LOG.debug("call to greeting")
         return Greeting("Hallo von Kotlin")
     }
 }
