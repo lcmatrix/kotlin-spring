57fb145e283e54157c3f46bda7f2fa543105831d
Norman <norman.seidel@gmail.com>
2019-05-20 15:56:24 +0200

    Add a POST endpoint to SampleController

--- src/main/kotlin/de/kotlin/spring/SampleController.kt
+++ src/main/kotlin/de/kotlin/spring/SampleController.kt
@@ -1,18 +1,29 @@
 package de.kotlin.spring
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 @RestController
+@RequestMapping("/greeting")
 class SampleController {
     private val LOG: Logger = LoggerFactory.getLogger(SampleController::class.java)
+    private var greets = Greeting("Default")
 
-    @GetMapping("/")
+    @GetMapping
     fun hello(): Greeting {
-        LOG.debug("call to greeting")
-        return Greeting("Hallo von Kotlin")
+        LOG.debug("Response: {}", greets)
+        return greets
+    }
+
+    @PostMapping
+    fun save(@RequestBody greeting: Greeting) {
+        LOG.debug("Got request body: {}", greeting)
+        greets = greeting
     }
 }
 
