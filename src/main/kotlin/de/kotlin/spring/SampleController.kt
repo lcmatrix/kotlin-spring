04e5582a180862286e57ec4060f863c449f8ee99
Norman Seidel <norman.seidel@gmail.com>
2020-04-07 22:24:55 +0200

    Methods for getting and saving persons

--- src/main/kotlin/de/kotlin/spring/SampleController.kt
+++ src/main/kotlin/de/kotlin/spring/SampleController.kt
@@ -1,5 +1,6 @@
 package de.kotlin.spring
 
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -8,22 +9,36 @@ import org.springframework.web.bind.annotation.RestController
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+import de.kotlin.spring.domain.Person
+import de.kotlin.spring.domain.PersonRepository
+
 @RestController
-@RequestMapping("/greeting")
-class SampleController {
+class SampleController(private val personRepository: PersonRepository) {
     private val LOG: Logger = LoggerFactory.getLogger(SampleController::class.java)
     private var greets = Greeting("Default")
 
-    @GetMapping
+    @GetMapping("/greeting")
     fun hello(): Greeting {
         LOG.debug("Response: {}", greets)
         return greets
     }
 
-    @PostMapping
+    @PostMapping("/greeting")
     fun save(@RequestBody greeting: Greeting) {
         LOG.debug("Got request body: {}", greeting)
         greets = greeting
+    }
+
+    @GetMapping("/person/{lastname}")
+    fun getPerson(@PathVariable lastname: String): Person? {
+        LOG.debug("searching for {}", lastname)
+        return personRepository.findByLastname(lastname)
+    }
+
+    @PostMapping("/person")
+    fun postPerson(@RequestBody person: Person) {
+        LOG.debug("inserting {}", person)
+        personRepository.insert(person)
     }
 }
 
